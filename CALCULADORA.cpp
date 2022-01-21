9aa5059031a9734c9e05ba7762690383fcfb38c8
Frogout2022 <milhos.sihuay@gmail.com>
2022-01-21 01:11:10 -0500

    update

--- CALCULADORA.cpp
+++ CALCULADORA.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
-
+void opciones(){
+	
+}
 int main(int argc, char *argv[]) {
-	cout<<"CALCULADORA";
-	cout<<"1. SUMAR";
-	cout<<"2. RESTAR";
-	cout<<"3. MULTIPLICAR";
-	cout<<"4. DIVIDIR";
-	cout<<"5. SALIR";
-	
 	int opc=0;
-	double num1=0,num2=0;
-	cout<<"\nIngrese opcion: ";
-	cin>>opc;
 	
-	cout<<"\nIngrese 1er número: "; cin>>num1;
-	cout<<"Ingrese 2do número: "; cin>>num2;
 	do{
+		double num1=0,num2=0;
+		cout<<"\t\n   CALCULADORA";
+		cout<<"\n1. SUMAR";
+		cout<<"\n2. RESTAR";
+		cout<<"\n3. MULTIPLICAR";
+		cout<<"\n4. DIVIDIR";
+		cout<<"\n5. SALIR";
+		
+		
+		
+		cout<<"\n\nIngrese opcion: ";
+		cin>>opc;
+		
+		cout<<"\nIngrese 1er número: "; cin>>num1;
+		cout<<"Ingrese 2do número: "; cin>>num2;
 		switch(opc){
 		case 1:
 			cout<<"\nEl resultado de la suma es: "<<num1+num2;
@@ -28,7 +33,8 @@ int main(int argc, char *argv[]) {
 			break;
 		case 4:
 			if(num2==0) cout<<"NO SE PUEDE DIVIDIR ENTRE 0!";
-			else cout<<"El resultado de la división es: "<<num1/num2;break
+			else cout<<"El resultado de la división es: "<<num1/num2;break;
+		default: break;
 		}
 	} while(opc!=5);
 	return 0;
