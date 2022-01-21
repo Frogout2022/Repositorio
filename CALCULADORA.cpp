7817e5c71b2168aa10ffc76994df7596ff629a71
Frogout2022 <milhos.sihuay@gmail.com>
2022-01-21 00:51:57 -0500

    Update CALCULADORA.cpp

--- CALCULADORA.cpp
+++ CALCULADORA.cpp
@@ -9,8 +9,21 @@ int main(int argc, char *argv[]) {
 	cout<<"4. DIVIDIR";
 	cout<<"5. SALIR";
 	
+	int opc=0;
+	cout<<"\nIngrese opcion: ";
+	cin>>opc;
+	cin>>
 	do{
-		
-	} while();
+		switch(opc){
+		case 1:
+			break;
+		case 2:
+			
+		case 3:
+			
+		case 4:
+				
+		}
+	} while(opc!=5);
 	return 0;
 }
